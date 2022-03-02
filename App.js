dc83b482f0c237fd57aec58bc3d7b3e5ea5c18e6
jinheebyeon <wlsgml8989@gmail.com>
2022-03-02 23:53:23 +0900

    add temperature

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Dimention, Text, View, ScrollView, Dimensions } from 'react-native';
+import { StyleSheet, Dimention, Text, View, ScrollView, Dimensions,ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location';
 
 
@@ -25,9 +25,9 @@ export default function App() {
     );
 
     setCity(location[0].city);
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alert&appid=${API_KEY}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alert&appid=${API_KEY}&units=metric`);
     const json = await response.json();
-    //setDays(json.daily);
+    setDays(json.daily);
 
   }
   useEffect(()=>{ask();},[])
@@ -38,7 +38,18 @@ export default function App() {
       <Text style = {styles.cityName}>{city}</Text>
     </View>
     <ScrollView pagingEnabled showsHorizontalScrollIndicator={false} horizontal contentContainerStyle = {styles.wheather}>
-      {days.length === 0 ? "" : (<View style = {styles.day}></View>)}
+      {days.length === 0 ? (
+      <View style = {styles.day}>
+        <ActivityIndicator color="white" style={{marginTop:10}}/>
+      </View>) : (
+        days.map((day,index) => 
+        <View key={index} style = {styles.day}>
+          <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+          <Text style={styles.desc}>{day.wheather[0].main}</Text>
+          <Text style={styles.tinyText}>{day.wheather[0].description}</Text>
+        </View>
+        )
+      )}
     </ScrollView>
     
   </View>
@@ -75,6 +86,9 @@ const styles = StyleSheet.create({
   desc : {
     fontSize:60
 
+  },
+  tinyText : {
+    fontSize:20
   }
 })
 
